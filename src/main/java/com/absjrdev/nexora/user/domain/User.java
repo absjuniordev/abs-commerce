da1895c64fed44649d08f357298d2d601789639b
Arnaldo Junior <abs.junnior@hotmail.com>
2026-05-26 21:34:14 -0300

    H2 database, test profile, JPA

--- src/main/java/com/absjrdev/nexora/user/domain/User.java
+++ src/main/java/com/absjrdev/nexora/user/domain/User.java
@@ -1,14 +1,17 @@
 package com.absjrdev.nexora.user.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public
 class User implements Serializable { //cadeia de bytes
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
